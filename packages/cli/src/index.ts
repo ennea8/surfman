be37a4e4891dff0557c20cbb14d02e8b9bfb6c76
ennea8 <ennea8@gmail.com>
2025-12-31 20:33:31 +0800

    optimize cli style

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import { createCheatcodesCommand } from './commands/cheatcodes';
+import { createTimeTravelCommand } from './commands/cheatcodes/time-travel';
+import { createSetAccountCommand } from './commands/cheatcodes/set-account';
+import { createSetProgramAuthorityCommand } from './commands/cheatcodes/set-program-authority';
 
 const program = new Command();
 
@@ -9,6 +11,8 @@ program
   .description('CLI tool for SurfPool RPC API interaction')
   .version('0.1.0');
 
-program.addCommand(createCheatcodesCommand());
+program.addCommand(createTimeTravelCommand());
+program.addCommand(createSetAccountCommand());
+program.addCommand(createSetProgramAuthorityCommand());
 
 program.parse();
